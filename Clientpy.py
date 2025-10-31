fc11b1c49b9c217da2ca16051fd29c1dba4aff6d
ronya <omeratt3509@gmail.com>
2025-10-31 21:10:29 +0200

    Revamped the entire project due to multiple issues.

--- Clientpy.py
+++ Clientpy.py
@@ -1,20 +1,58 @@
 import socket
+import logging
+import os
+"""
+Server Project-
+Made: 2025
+By: Omer Attia
 
+This is the client side program it has its own log and will ensure no unkown
+ commands are inputted into the server. Also ensures the client disconnects properly\
+ log file: clients.log
+"""
 MAX_PACKET = 1024
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    my_socket.connect(('127.0.0.1', 8820))
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 8820
+LOG_FILE = os.path.join(os.path.dirname(__file__), 'clients.log')
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
-    while True:
-        print("Please insert the following commands: TIME, RAND, NAME, EXIT")
-        X = input()
-        my_socket.send(X.encode())
-        response = my_socket.recv(MAX_PACKET).decode()
-        if X == "EXIT":
-            my_socket.close()
-            break
-        print(response)
-except socket.error as err:
-    print('received socket error ' + str(err))
-finally:
-    my_socket.close()
+def main():
+    logging.info("Client started")
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client_socket.connect((SERVER_IP, SERVER_PORT))
+        logging.info("Client connected to server" + SERVER_IP + ":" + str(SERVER_PORT))
+
+        while True:
+            cmd = input("Enter command: (TIME, NAME, RAND, EXIT): ").strip().upper()
+            if not cmd:
+                print("Empty command, please try again.")
+                logging.warning("Empty command.")
+                continue
+
+            assert len(cmd) <= 4, f"Command '{cmd}' is too long. Max length is 4 characters."
+            client_socket.send(cmd.encode())
+            data = b""
+
+
+            response = client_socket.recv(MAX_PACKET).decode()
+            print("Server:" + response)
+            logging.info(response)
+
+            if cmd == "EXIT":
+                logging.info("Server exited.")
+                break
+    except AssertionError as e:
+        logging.error(e)
+    except socket.error as err:
+        logging.error(err)
+    finally:
+        client_socket.close()
+if __name__ == "__main__":
+    main()
+
--- Serverpy.py
+++ Serverpy.py
@@ -14,60 +14,84 @@ The server will have 4 outputs availble:
 2.  random number
 3. The name of the device the server is hosted on
 4. A command that allows the client to disconnect from the server
-All logs will be put in 'server.log'
+All logs will be put in 'serverpys.log'
 """
-NOW = datetime.now()
 SERVER_NAME = socket.gethostname()
+IP = "0.0.0.0"
+PORT = 8820
 QUEUE_LEN = 1
 MAX_PACKET = 1024
-LOG_PATH = os.path.join(os.path.dirname(__file__), 'serverpy.log')
-logging.basicConfig(filename=LOG_PATH, level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', force=True)
+LOG_PATH = os.path.join(os.path.dirname(__file__), 'serverpys.log')
+logging.basicConfig(
+    filename=LOG_PATH,
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    force=True
+)
 
 
+
+def handle_command(command: str) -> str:
+    """Processes the 4 byte command and returns the appropriate response"""
+    assert isinstance(command, str),"Command must be a string"
+    assert len(command) <= 4, "Command must be less then 4 letters"
+    if command == 'TIME':
+        logging.info('Sent local time')
+        return datetime.now().strftime("%H:%M:%S")
+    elif command == 'EXIT':
+        logging.info('Client requested to exit the server')
+        return "Bye!"
+    elif command == 'NAME':
+        logging.info('Sent server name')
+        return SERVER_NAME
+    elif command == 'RAND':
+        num = random.randint(1, 10)
+        logging.info('Sent random number between 1 and 10')
+        return str(num)
+    else:
+        logging.error('Unknown command received: {command}')
+        return "Unknown command"
 
 def main():
+    logging.info('Server started')
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, PORT))
+    server_socket.listen(QUEUE_LEN)
+
+    logging.info(f'Server listening on port: {PORT}')
     try:
-        server_socket.bind(('0.0.0.0', 8820))
-        server_socket.listen(QUEUE_LEN)
-        logging.info('Listening on port 8820')
         while True:
             client_socket, client_address = server_socket.accept()
-            logging.info('Accepted connection from {}'.format(client_address))
+            logging.info('Client connected')
+
             try:
                 while True:
-                    handled = True
-                    response = "Error unkown command given"
-                    request = client_socket.recv(MAX_PACKET).decode().strip()
-                    assert len(request) <= 4, f"Command too long: {len(request)}"
-                    logging.info('Received {}'.format(request))
-                    if request == 'TIME':
-                        response = datetime.now().strftime("%H:%M:%S")
-                        logging.info('Sent local time')
-                    elif request == 'EXIT':
-                        client_socket.close()
-                        logging.info('Server exited')
-                    elif request == 'NAME':
-                        response = ("Server name :"+SERVER_NAME)
-                        logging.info('Sent server name')
-                    elif request == 'RAND':
-                        response = f'Random number: {random.randint(1, 10)}'
-                        logging.info('Sent random number generator')
-                    elif request == "QUEUE":
-                        response = "Queue :" + str(QUEUE_LEN)
-                        logging.info('Sent queue length')
-                    else:
-                        handled = False
-                    if not handled:
-                        logging.error('Received unknown command')
-                    client_socket.send(response.encode())
-            except socket.error as err:
-                logging.error('Socket error {}'.format(err))
+                    try:
+                        data = client_socket.recv(MAX_PACKET).decode().strip()
+                    except socket.error as err:
+                        logging.error(f"Socket error: {err}")
+                        break
+                    if not data:
+                        logging.info('Client disconnected')
+                    logging.info(f"Data received: {data}")
+                    try:
+                        response = handle_command(data)
+                        client_socket.send(response.encode())
+                    except AssertionError as e:
+                        response = str(e)
+                        logging.error(f"Exception raised: {e}")
 
-    except socket.error as err:
-        logging.error('Socket error {}'.format(err))
+                    if data == 'EXIT':
+                        logging.info('Client disconnected')
+                        client_socket.close()
+            finally:
+                client_socket.close()
     finally:
         server_socket.close()
-        logging.info('Closing server')
+        logging.info('Server closed')
 if __name__ == "__main__":
     main()
+
+
+
+
